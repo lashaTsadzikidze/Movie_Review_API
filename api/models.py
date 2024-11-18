c8699a6a1f3266479bba5e188ef9b857861e9e50
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-18 18:38:42 +0400

    create a movie model and make the migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    description = models.TextField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
