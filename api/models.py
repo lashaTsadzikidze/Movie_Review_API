da87b3d5554bb2236c540befd12bec9d773c0abb
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-18 19:44:28 +0400

    create a review model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,13 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.movie.title} ({self.rating} / 10)'
