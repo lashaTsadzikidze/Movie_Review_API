8bbfa19cf32219dd5966c32430fd1def756c1e27
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-18 19:52:25 +0400

    create serializer for the Review model

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ['id', 'user', 'rating', 'comment', 'created_at']
 
 class MovieSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'genre', 'description', 'release_date']
+        fields = ['id', 'title', 'genre', 'description', 'release_date', 'reviews']
