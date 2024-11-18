d605cba36b17a2d43af2dfcf45064d277380fe36
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-18 18:56:41 +0400

    create a view for a list of movies

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
 
 # Create your views here.
+class MovieListView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
