ad1c4f3b6d83766048a74fa737ed23651d5bc837
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-20 19:43:24 +0400

    add A ReviewDestroyView and a permissions for each view

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import MovieListView, MovieDetailView
+from django.urls import path, include
+from .views import MovieListView, MovieDetailView, ReviewCreateView, ReviewDestroyView
 
 urlpatterns = [
     path('movies/', MovieListView.as_view(), name='movie-list'),
     path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie_detail'),
+    path('movies/<int:movie_id>/reviews/', ReviewCreateView.as_view(), name='add-review'),
+    path('movies/<int:movie_id>/reviews/<int:pk>/', ReviewDestroyView.as_view(), name='delete-view'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,33 @@
 from rest_framework import generics
-from .models import Movie
-from .serializers import MovieSerializer
+from .models import Movie, Review
+from .serializers import MovieSerializer, ReviewSerializer
+from .permissions import IsAdminOrReadOnly
+from rest_framework import permissions
+from rest_framework.exceptions import NotFound
 
 # Create your views here.
-class MovieListView(generics.ListAPIView):
+class MovieListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
-class MovieDetailView(generics.RetrieveAPIView):
+class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class ReviewCreateView(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        movie = Movie.objects.get(pk=self.kwargs['movie_id'])
+        serializer.save(user=self.request.user, movie=movie)
+
+class ReviewDestroyView(generics.DestroyAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        return Review.objects.filter(pk=pk)
